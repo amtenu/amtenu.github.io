8aff89def6a40fd13db0e2e3094dc1ba3ba66576
amtenu <amannov21@gmail.com>
2022-08-04 09:45:22 -0600

    Home component done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -46,7 +46,7 @@ const NavBar = () => {
       </ul>
 
       {/* Social Media */}
-      <div className="flex fixed flex-col top-[35%] left-0">
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 ">
             <a
@@ -56,7 +56,7 @@ const NavBar = () => {
               LinkedIn <FaLinkedin size={30} />
             </a>
           </li>
-          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 ">
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-black-600 ">
             <a
               href="/"
               className="flex justify-between items-center w-full text-gray-300"
@@ -64,7 +64,7 @@ const NavBar = () => {
               GitHub <FaGithub size={30} />
             </a>
           </li>
-          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 ">
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-yellow-600 ">
             <a
               href="/"
               className="flex justify-between items-center w-full text-gray-300"
@@ -72,7 +72,7 @@ const NavBar = () => {
               Email <HiOutlineMailOpen size={30} />
             </a>
           </li>
-          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 ">
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-600 ">
             <a
               href="/"
               className="flex justify-between items-center w-full text-gray-300"
