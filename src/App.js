d97259db6627067dce2a992a37225106c4ec0813
amtenu <amannov21@gmail.com>
2022-08-04 19:44:29 -0600

    Skills and Proje

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import About from "./components/About";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
+      <About/>
+      <Skills/>
+      <Projects />
+
     </div>
   );
 }
