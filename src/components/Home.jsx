22ceea7b836b23d9fdf352ffb72babce531a73a6
amtenu <amannov21@gmail.com>
2022-08-04 19:45:10 -0600

    Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,14 +5,14 @@ const Home = () => {
   return (
     <div name="home" className="w-full h-screen bg-[#0a192f]">
       <div className=" max-w-[1000px] mx-auto px-8   flex flex-col  justify-center  h-full">
-        <p className="text-pink-600">Hi,My Name is </p>
-        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+        <p className=" text-orange-400 text-3x1   ">Hi,My Name is </p>
+        <h1 className="text-4xl sm:text-6xl font-bold text-[#ccd6f6]">
           Amanuel Nigussie
         </h1>
-        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+        <h2 className="text-4xl sm:text-3xl font-bold text-[#8892b0]">
           I am a Full Stack Web Developer
         </h2>
-        <p className="text-[#8892b0] py-4 max-w-[700px]">
+        <p className="text-[#8892b0] py-4 max-w-[500px]">
           I am a Full Stack Web Developer with a passion for creating beautiful
           and intuitive user experiences.
         </p>
